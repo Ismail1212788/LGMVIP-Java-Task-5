b45880fcc1d79fef08476f83b369e4dc444c7272
Muhammad Ismail <muhammadismaillashari0@gmail.com>
2023-09-17 00:30:26 -0700

    initial commit

---
+++ src/main/java/com/mycompany/texteditor/TextEditor.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.texteditor;
+
+/**
+ *
+ * @author ali
+ */
+public class TextEditor {
+
+    public static void main(String[] args) {
+       UIFrame a=new UIFrame();
+       a.show();
+    }
+}
